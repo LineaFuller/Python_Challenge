938c4fdf41ba4245059987f9aefdacd410dc2439
LineaFuller <lineafuller2@gmail.com>
2021-10-21 20:17:46 -0700

    Analysis

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -4,10 +4,6 @@ import csv
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
 
-
-
-
-
 # read in the csv file
 with open (budget_csv, 'r') as csv_file: 
     csv_reader = csv.reader(csv_file, delimiter = ",")
